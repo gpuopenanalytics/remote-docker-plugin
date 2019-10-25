016e2162741091230198d0384536ad6bc34102fe
Raymond Douglass <ray@raydouglass.com>
2019-10-25 02:38:51 -0400

    FEA Override --workdir

--- src/main/java/com/gpuopenanalytics/jenkins/remotedocker/DockerLauncher.java
+++ src/main/java/com/gpuopenanalytics/jenkins/remotedocker/DockerLauncher.java
@@ -136,6 +136,9 @@ public class DockerLauncher extends Launcher {
     private ArgumentListBuilder getlaunchArgs(DockerConfiguration config,
                                               boolean isMain) throws IOException, InterruptedException {
         String workspacePath = build.getWorkspace().getRemote();
+        String workspaceTarget = Optional.ofNullable(
+                buildWrapper.getWorkspaceOverride())
+                .orElse(workspacePath);
         //Fully resolve the source workspace
         String workspaceSrc = Paths.get(workspacePath)
                 .toAbsolutePath()
@@ -159,7 +162,6 @@ public class DockerLauncher extends Launcher {
         //TODO Set name? Maybe with build.toString().replaceAll("^\\w", "_")
         ArgumentListBuilder args = new ArgumentListBuilder()
                 .add("run", "-t", "-d")
-                .add("--workdir", workspacePath)
                 //Add bridge network for internet access
                 .add("--network", "bridge");
         //Add inter-container network if needed
@@ -168,8 +170,9 @@ public class DockerLauncher extends Launcher {
         if (isMain) {
             //Start a shell to block the container, overriding the entrypoint in case the image already defines that
             args.add("--entrypoint", "/bin/sh")
-                    .add("-v", workspaceSrc + ":" + workspacePath)
-                    ////Jenkins puts scripts here
+                    .add("--workdir", workspaceTarget)
+                    .add("-v", workspaceSrc + ":" + workspaceTarget)
+                    //Jenkins puts scripts here
                     .add("-v", tmpSrc + ":" + tmpDest);
         }
         config.addCreateArgs(this, args, build);
@@ -251,7 +254,10 @@ public class DockerLauncher extends Launcher {
         ArgumentListBuilder args = new ArgumentListBuilder()
                 .add("exec");
         if (starter.pwd() != null) {
-            args.add("--workdir", starter.pwd().getRemote());
+            String path = Optional.ofNullable(
+                    buildWrapper.getWorkspaceOverride())
+                    .orElse(starter.pwd().getRemote());
+            args.add("--workdir", path);
         }
         if (addRunArgs) {
             buildWrapper.getDockerConfiguration().addRunArgs(this, args, build);
@@ -260,6 +266,10 @@ public class DockerLauncher extends Launcher {
         args.add(mainContainerId);
 
         args.add("env").add(starter.envs());
+        if (buildWrapper.getWorkspaceOverride() != null) {
+            //Override $WORKSPACE inside the container
+            args.add("WORKSPACE=" + buildWrapper.getWorkspaceOverride());
+        }
 
         List<String> originalCmds = starter.cmds();
         boolean[] originalMask = starter.masks();
